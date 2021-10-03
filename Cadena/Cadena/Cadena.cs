5cce713f2c79cf7a220f3987fed44e8a4633ef75
rosalindaaap <82152732+rosalindaaap@users.noreply.github.com>
2021-10-03 12:03:12 -0400

    Add files via upload

---
+++ Cadena/Cadena/Cadena.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadenas
+{
+    class Cadena
+    {
+        private string Frase;
+
+        public void Operacion()
+        {
+            Console.Write("Ingrese la frase: ");
+            Frase = Console.ReadLine();
+
+            int i = Frase.Length;
+            int j = 0;
+
+            while (j < i)
+            {
+                Frase = Frase.Replace(" ", "\n");
+                Console.Write(Frase[j]);
+                j++;
+            }
+            Console.Write("\n");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cadena n = new Cadena();
+            n.Operacion();
+        }
+    }
+}
